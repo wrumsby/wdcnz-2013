f933dd674653410b489c724ee444d5dd34fb0be4
Walter Rumsby <walter@rumsby.net>
2013-06-25 20:32:47 +1200

    second commit

---
+++ slides.js
@@ -0,0 +1,90 @@
+var SLIDES = [
+	{
+		"cls": "first",
+		"title": "Hi",
+		"notes": "Who am I?"
+	},
+
+	{
+		"cls": "first",
+		"title": "The Loading Point: How Little Modules Can Make a Big Difference",
+		"lanyrd": "http://lanyrd.com/scfkyb",
+		"hashtag": "jsmod",
+		"notes": "What is this about? This is opinion"
+	},
+
+	{
+		"quote": {
+			"text": "The worst thing that can happen to a code base is size",
+			"from": "Steve Yegge"
+		},
+		"image": "http://www.flickr.com/photos/zapthedingbat/517644777/",
+		"notes": "http://steve-yegge.blogspot.co.nz/2007/12/codes-worst-enemy.html"
+	},
+
+	{
+		"quote": {
+			"text":	"The secret to building large apps is never build large apps. Break your applications into small pieces. Then, assemble those testable,  bite-sized pieces into your big application",
+			"from":	"Justin Meyer, JavaScript MVC"
+		},
+		"image": "http://www.flickr.com/photos/62472560@N00/4679740934/"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/feverblue/3865671508/",
+		"notes": "modules help us break big apps into those bite-sized pieces"
+	},
+
+	{
+		"quote": {
+			"text": "module: a unit of source code with optional imports and exports",
+			"from": "John Hann via Yehuda Katz"
+		},
+		"image": "http://www.flickr.com/photos/sayamindu/5677674922/"
+	},
+
+	{
+		"tweet": "https://twitter.com/keranm/status/315670961594699777"
+	},
+
+	{
+		"quote": {
+			"text": "loader: an object that defines how modules are fetched, translated and compiled into a module instance object",
+			"from": "John Hann via Yehuda Katz"
+		},
+		"image": "http://www.flickr.com/photos/moehre1992/8436105600/"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/insouciance/3061759623/",
+		"notes": "seems more of a conclusion thing"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/49512158@N00/6231468612/",
+		"title": "Thank you!",
+		"list": ["@evangoer", "@davglass", "@unscriptable", "@jrburke", "@souders"],
+		"notes": "@davglass, @evangoer, @yaypie, @kiwipom"
+	},
+	{
+		"cls": "xero",
+		"html": "<span class=\"logo\"></span>",
+		"link": "http://xero.com/careers"
+	},
+
+	{
+		"cls": "javascript",
+		"text": "JS",
+		"link": "http://meetup.com/AucklandJS/"
+	},
+
+	{
+		"credits": true
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/24046097@N00/3908013145/",
+		"cls": "last",
+		"twitter": "wrumsby"
+	}
+];
